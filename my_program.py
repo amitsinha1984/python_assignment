b9447fab67e0af24e61943c28ee30d0172bb4cd6
amitsinha1984 <amit.sinha769252in@gmail.com>
2026-01-05 08:20:17 -0600

    Add files via upload
    my_program.py file contains first task of Arithmetic Operations and greeting.py contain the task-2 of personalized greeting.

---
+++ my_program.py
@@ -0,0 +1,12 @@
+num1=input("enter first number: ")
+num2=input("enter second number: ")
+
+add_num=int(num1)+int(num2)
+sub_num=int(num1)-int(num2)
+mul_num=int(num1)*int(num2)
+div_num=int(num1)/int(num2)
+
+print("Addition",add_num)
+print("Subtraction",sub_num)
+print("Multiplication",mul_num)
+print("Division",div_num)
